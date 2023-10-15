c59d5f6adb00f0e8d9fc4d6e1cf4d03e8c83711b
SABAHMOHAMEDD <102183714+SABAHMOHAMEDD@users.noreply.github.com>
2023-10-15 17:51:20 +0300

    details new font

--- lib/view/details_view.dart
+++ lib/view/details_view.dart
@@ -62,141 +62,177 @@ class DetailsView extends StatelessWidget {
               ],
             ),
             Expanded(
-                flex: 5,
-                child: SizedBox(
-                  height: MediaQuery.of(context).size.height,
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const SizedBox(
-                          height: 20,
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 0),
+                        child: Text(
+                          productModel.name ?? "",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 26,
+                              fontFamily: 'SFProDisplay',
+                              color: Colors.black.withOpacity(0.8)),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 24),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                productModel.name ?? "",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 18,
-                                    color: Colors.black.withOpacity(0.8)),
-                              ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              ExpandableText(
-                                productModel.description ?? "",
-                                expandText: 'show more',
-                                collapseText: 'show less',
-                                linkColor: KPrimaryColor,
-                                maxLines: 3,
-                                style: const TextStyle(
-                                    fontSize: 14,
-                                    color: Colors.grey,
-                                    letterSpacing: 0.5),
-                              )
-                            ],
-                          ),
-                        ),
-                        const Spacer(),
-                        Row(
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 30),
-                              child: SizedBox(
-                                  width: 100,
-                                  height: 60,
-                                  child: Center(
-                                      child: Column(
+                            Container(
+                              height: 45,
+                              width: MediaQuery.of(context).size.width * .45,
+                              decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.grey),
+                                  borderRadius: BorderRadius.circular(8)),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  const Text('Size'),
+                                  Text(productModel.size ?? "")
+                                ],
+                              ),
+                            ),
+                            Container(
+                              height: 45,
+                              width: MediaQuery.of(context).size.width * .45,
+                              decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.grey),
+                                  borderRadius: BorderRadius.circular(8)),
+                              child: const Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Text('Color'),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      const Text(
+                        'Details',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 0),
+                        child: ExpandableText(
+                          productModel.description ?? "",
+                          expandText: 'show more',
+                          collapseText: 'show less',
+                          linkColor: KPrimaryColor,
+                          maxLines: 3,
+                          style: const TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey,
+                              letterSpacing: 0.5),
+                        ),
+                      ),
+                      Spacer(),
+                      Row(
+                        children: [
+                          SizedBox(
+                              width: 100,
+                              height: 60,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Text(
+                                    'Total Price',
+                                    style: TextStyle(
+                                        color: Colors.grey,
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                  Row(
                                     mainAxisAlignment: MainAxisAlignment.center,
                                     children: [
-                                      const Text(
-                                        'Total Price',
+                                      Text(
+                                        '\$',
                                         style: TextStyle(
-                                            color: Colors.grey,
-                                            fontSize: 12,
-                                            fontWeight: FontWeight.w600),
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w500,
+                                            color: KSecondryColor),
                                       ),
-                                      Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          Text(
-                                            '\$',
-                                            style: TextStyle(
-                                                fontSize: 18,
-                                                fontWeight: FontWeight.w500,
-                                                color: KSecondryColor),
-                                          ),
-                                          Text(
-                                            productModel.price ?? "",
-                                            style: TextStyle(
-                                                fontSize: 18,
-                                                fontWeight: FontWeight.w500,
-                                                color: KSecondryColor),
-                                          ),
-                                        ],
+                                      Text(
+                                        productModel.price ?? "",
+                                        style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w500,
+                                            color: KSecondryColor),
                                       ),
                                     ],
-                                  ))),
-                            ),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 24),
-                              child: Container(
-                                width: 200,
-                                height: 60,
-                                decoration: BoxDecoration(
-                                    borderRadius: const BorderRadius.only(
-                                        topRight: Radius.circular(20),
-                                        bottomRight: Radius.circular(20),
-                                        bottomLeft: Radius.circular(20)),
-                                    color: KPrimaryColor.withOpacity(0.74)),
-                                child: MaterialButton(
-                                  onPressed: () {},
-                                  child: const Padding(
-                                    padding:
-                                        EdgeInsets.symmetric(horizontal: 24),
-                                    child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.end,
-                                      children: [
-                                        Icon(
-                                          Icons.shopping_cart_outlined,
-                                          size: 19,
-                                          color: Colors.white,
-                                        ),
-                                        SizedBox(
-                                          width: 4,
-                                        ),
-                                        Text(
-                                          'Add To Cart',
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.w500,
-                                              color: Colors.white,
-                                              fontSize: 15),
-                                        ),
-                                      ],
-                                    ),
                                   ),
+                                ],
+                              )),
+                          Spacer(),
+                          Container(
+                            width: 200,
+                            height: 60,
+                            decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                    topRight: Radius.circular(20),
+                                    bottomRight: Radius.circular(20),
+                                    bottomLeft: Radius.circular(20)),
+                                color: KPrimaryColor),
+                            child: MaterialButton(
+                              onPressed: () {},
+                              child: const Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 24),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Icon(
+                                      Icons.shopping_cart_outlined,
+                                      size: 19,
+                                      color: Colors.white,
+                                    ),
+                                    SizedBox(
+                                      width: 4,
+                                    ),
+                                    Text(
+                                      'Add To Cart',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w500,
+                                          color: Colors.white,
+                                          fontSize: 15),
+                                    ),
+                                  ],
                                 ),
                               ),
                             ),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 30,
-                        )
-                      ],
-                    ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      )
+                    ],
                   ),
-                )),
+                ),
+              ),
+            ),
           ],
         ),
       ),
